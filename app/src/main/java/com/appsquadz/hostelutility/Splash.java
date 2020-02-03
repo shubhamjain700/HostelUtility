30f3659281d86c7948a6871c0e56080f6f9795e8
Gaurav Sharma <gauravvishwakarma0007@gmail.com>
2020-02-03 16:28:11 +0530

    splash screen

--- app/src/main/java/com/appsquadz/hostelutility/Splash.java
+++ app/src/main/java/com/appsquadz/hostelutility/Splash.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
-
-
+        setContentView(R.layout.activity_splash);
     }
 }
