7483e2c116023adca009786af8eef7dd3833181e
shubhamjain700 <shubham.jain_cs17@gla.ac.in>
2020-04-04 12:09:28 +0530

    Presenter

---
+++ app/src/main/java/com/appsquadz/hostelutility/IODashboardPresenter.java
@@ -0,0 +1,10 @@
+package com.appsquadz.hostelutility;
+
+import com.code_base_update.presenters.IBasePresenter;
+
+public interface IODashboardPresenter extends IBasePresenter<IODashboardView> {
+    void loadData();
+
+    void loadProfileImage();
+
+}
