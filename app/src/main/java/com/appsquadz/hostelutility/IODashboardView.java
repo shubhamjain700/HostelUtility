b9e945346e6b6af2e2808f6fb03ca7007d256470
shubhamjain700 <shubham.jain_cs17@gla.ac.in>
2020-04-04 19:01:38 +0530

    Dashboard

--- app/src/main/java/com/appsquadz/hostelutility/IODashboardView.java
+++ app/src/main/java/com/appsquadz/hostelutility/IODashboardView.java
@@ -1,11 +1,11 @@
-//package com.appsquadz.hostelutility;
-//
-//import com.code_base_update.beans.DashBoardBean;
-//import com.code_base_update.view.IBaseView;
-//
-//import java.util.ArrayList;
-//
-//public interface IODashboardView extends IBaseView {
-//    void onDataLoaded(ArrayList<DashBoardBean> list);
-//    void onProfileImageLoaded();
-//}
+package com.appsquadz.hostelutility;
+
+import com.code_base_update.beans.DashBoardBean;
+import com.code_base_update.view.IBaseView;
+
+import java.util.ArrayList;
+
+public interface IODashboardView extends IBaseView {
+    void onDataLoaded(ArrayList<DashBoardBean> list);
+    void onProfileImageLoaded();
+}
