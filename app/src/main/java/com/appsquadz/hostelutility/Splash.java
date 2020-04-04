943f5b8fb5ecf42e9ea76cff51135782f38e6f4a
shubhamjain700 <shubham.jain_cs17@gla.ac.in>
2020-04-04 12:04:23 +0530

    Splash

--- app/src/main/java/com/appsquadz/hostelutility/Splash.java
+++ app/src/main/java/com/appsquadz/hostelutility/Splash.java
@@ -2,6 +2,7 @@ package com.appsquadz.hostelutility;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        startActivity(new Intent(this, Dashboard.class));
+        finish();
     }
 }
